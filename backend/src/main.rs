aaa352e1f6507ce8cd42410cdd160514ee57b54f
sul <su1im69n@gmail.com>
2024-09-01 08:39:55 +0300

    add pg_pool

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,10 @@
 use actix_web::{get, guard, post, web, App, HttpResponse, HttpServer, Responder};
-use db::init_pool;
+use db::{init_pool, PgPool, PgPooledConnection};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let pg_pool = init_pool();
+    let pg_pool = web::Data::new(init_pool());
+
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pg_pool.clone()))
@@ -12,13 +13,13 @@ async fn main() -> std::io::Result<()> {
             .route("/hey", web::get().to(manual_hello))
             .service(
                 web::scope("/")
-                    .guard(guard::Host("www.rust-land.org"))
+                    .guard(guard::Host("www.localhost"))
                     .route("", web::to(|| async { HttpResponse::Ok().body("www") })),
             )
             .service(
                 web::scope("/")
-                    .guard(guard::Host("www.rust-lang.org"))
-                    .route("", web::to(|| async { HttpResponse::Ok().body("www") })),
+                    .guard(guard::Host("users.localhost"))
+                    .route("", web::to(|| async { HttpResponse::Ok().body("users") })),
             )
             .route("/", web::to(HttpResponse::Ok))
     })
@@ -39,4 +40,9 @@ async fn echo(req_body: String) -> impl Responder {
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
+}
+
+pub fn pg_pool_handler(pool: web::Data<PgPool>) -> Result<PgPooledConnection, HttpResponse> {
+    pool.get()
+        .map_err(|e| HttpResponse::InternalServerError().json(e.to_string()))
 }
